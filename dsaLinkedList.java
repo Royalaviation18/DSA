0ba580c98fc544ece534bb5ed55ad24fc520e486
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-09-04 11:09:22 +0530

    Update dsaLinkedList.java

--- dsaLinkedList.java
+++ dsaLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 class Node {
     int data;
     Node next;
@@ -6,8 +8,90 @@ class Node {
         this.data = data;
     }
 
-    // reversing the linkedlist
+    static Node removeLoop(Node head) {
+        Node slow = head, fast = head;
+        while (fast!= null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+            if (slow == fast) {
+                slow = head;
+                while (slow.next != fast.next) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                fast.next = null;
+            }
+        
+        return head;
+    }
 
+    // detecting loops in linkedlist
+    static boolean detectLoops(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // removing duplicates in an unsorted linkedlist
+    static Node duplicateUnSorted(Node head) {
+        HashSet<Integer> hs = new HashSet<>();
+        Node cur = head, prev = null;
+        while (cur != null) {
+            int curVal = cur.data;
+            if (hs.contains(curVal)) {
+                prev.next = cur.next;
+            } else {
+                hs.add(curVal);
+                prev = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    // removing duplicates in a sorted Linked List
+    static Node duplicatesSorted(Node head) {
+        Node cur = head;
+        while (cur.next != null) {
+            if (cur.data == cur.next.data) {
+                Node temp = cur.next;
+                cur.next = cur.next.next;
+                temp = null;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    // recurssion palindrome (not working as of now)
+    static boolean isPalindrome(Node left, Node right) {
+        if (right == null) {
+            return true;
+        }
+        boolean first = isPalindrome(left, right.next);
+
+        if (first == false) {
+            return false;
+        }
+        boolean second = (right.data == left.data);
+        left = left.next;
+        return second;
+
+    }
+
+    // reversing the linkedlist
     static Node reverseLinkedList(Node head) {
         Node prev = null;
         Node curr = head, next = head;
@@ -119,7 +203,7 @@ class Node {
     }
 
     public static void main(String[] args) {
-        Node n1 = new Node(10);
+        Node n1 = new Node(50);
         Node n2 = new Node(20);
         Node n3 = new Node(30);
         Node n4 = new Node(40);
@@ -130,31 +214,52 @@ class Node {
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        n5.next = null;
+        n5.next = n2;
 
         // traversing through the linked list
-        traverse(head);
-        // Printing the middle Element
-        System.out.println(middleElement(head));
-        // returning head from deleteMidElement
-        head = deleteMidElement(head);
-        // printing the ll after removing the middle element
-        traverse(head);
+        // traverse(head);
+        // // Printing the middle Element
+        // System.out.println(middleElement(head));
+        // // returning head from deleteMidElement
+        // // // head = deleteMidElement(head);
+        // // // printing the ll after removing the middle element
+        // // traverse(head);
 
-        System.out.println("Deleting the entire LL");
-        // head = deleteLinkedList(head);
+        // // System.out.println("Deleting the entire LL");
+        // // // head = deleteLinkedList(head);
+        // // // traverse(head);
+
+        // // printing the reverse linkedList
+        // // head = printReverse(head);
+        // // System.out.println(reverseTraverse(head));
+        // // Printing the reverse of ll recursively without reversing or any additional
+        // // space
+        // System.out.println("\nPrinting reverse of linkedlist without reversing the
+        // ll\n");
+        // reverseTraverse(head);
+
+        // // System.out.println("\n Reverse of the linkedlist\n");
+        // // head = reverseLinkedList(head);
+        // // traverse(head);
+        // // System.out.println(isPalindrome(head, head)); not working
+
+        // // remove duplicates in a sorted linked list
+
+        // // head = duplicatesSorted(head);
+        // System.out.println("\nAfter removing duplicates in a sorted linkedlist\n");
+        // // traverse(head);
+
+        // head = duplicateUnSorted(head);
+        // System.out.println("\nAfter removing duplicates in an unsorted
+        // linkedlist\n");
         // traverse(head);
 
-        // printing the reverse linkedList
-        // head = printReverse(head);
-        // System.out.println(reverseTraverse(head));
-        // Printing the reverse of ll recursively without reversing or any additional
-        // space
-        System.out.println("\nPrinting reverse of linkedlist without reversing the ll\n");
-        reverseTraverse(head);
+        System.out.println("\n Does the linkedlist contains loop: ");
+        System.out.println(detectLoops(head));
 
-        System.out.println("\n Reverse of the linkedlist\n");
-        head = reverseLinkedList(head);
-        traverse(head);
+        head = removeLoop(head);
+        System.out.println("\nDoes the linkedlist contains loop now ? : ");
+        System.out.println(detectLoops(head));
+
     }
 }
