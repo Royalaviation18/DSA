408345a375c2d0ec5c29f0cc2896b2bc30660f17
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-09-05 20:30:05 +0530

    Update midLL.java

--- midLL.java
+++ midLL.java
@@ -1,4 +1,6 @@
-// Finding the middle Element in a Linked List
+//Finding middle Element 
+//Deleting middle Element
+
 
 class Node{
     int data;
@@ -31,25 +33,48 @@ class Node{
         return slow.data;
     }
 
+    public static Node delMidElement(Node head){
+        Node slow = head;
+        Node fast = head;
+        Node prev = slow;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+        slow = null;
+
+        return head;
+        
+    }
+
     public static void main(String[] args) {
         Node n1 = new Node(10);
         Node n2 = new Node(20);
         Node n3 = new Node(30);
         Node n4 = new Node(40);
         Node n5 = new Node(50);
+        Node n6 = new Node(60);
 
         Node head = n1;
         head.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        n5.next = null;
+        n5.next = n6;
+        n6.next = null;
 
         System.out.println("\n The linkedlist is now : ");
         traverse(head);
 
         System.out.println("\n The middle element is \n") ;
         System.out.println(midElement(head));
+
+        System.out.println("\nDeleting middle element....");
+        delMidElement(head);
+        System.out.println("\n The list is now\n");
+        traverse(head);
 
     }
 }
