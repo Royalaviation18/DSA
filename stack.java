7db26d8ee80a4c27643048e48ea979a68fbbf626
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-09-06 20:47:04 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,19 @@
+class sNode{
+    int data;
+    sNode  next;
+}
+
+class myStack{
+    sNode head;
+    int size;
+
+    public myStack(){
+        head = null;
+        size = 0;
+    }
+
+    public static void main(String[] args) {
+        sNode s1 = new sNode();
+            
+    }
+}
