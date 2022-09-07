96d5d7b4dcbd2bb1c4ca1e4985eb32091db885f0
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2022-09-07 07:10:43 +0530

    Update stack.java

--- stack.java
+++ stack.java
@@ -1,19 +1,70 @@
-class sNode{
+class sNode {
     int data;
-    sNode  next;
+    sNode next;
+
+    sNode(int data) {
+        this.data = data;
+    }
 }
 
-class myStack{
+class myStack {
     sNode head;
     int size;
 
-    public myStack(){
+    myStack() {
         head = null;
         size = 0;
     }
 
+    public void push(int data) {
+        sNode temp = new sNode(data);
+        temp.next = head;
+        size++;
+        head = temp;
+
+    }
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    public int peek() {
+        if (!isEmpty()) {
+            return head.data;
+        } else {
+            System.out.println("Stack is empty ");
+            return -1;
+        }
+    }
+
+    int pop() {
+        if (head == null) {
+            System.out.println("\nStack is empty\n");
+        }
+        int res = head.data;
+        head = head.next;
+        size--;
+        return res;
+    }
+
+    int sSize() {
+        return size;
+    }
+
     public static void main(String[] args) {
-        sNode s1 = new sNode();
-            
+        myStack s1 = new myStack();
+        System.out.println(s1.peek());
+        System.out.println("\n Is stack empty : " + s1.isEmpty());
+        System.out.println("\nSize of the stack : " + s1.sSize());
+        System.out.println("\nInserting data");
+        s1.push(10);
+        s1.push(20);
+        System.out.println("\nSize of the stack : " + s1.sSize());
+        s1.push(30);
+        System.out.println("\nSize of the stack : " + s1.sSize());
+        System.out.println("\nTop element of the stack: " + s1.peek());
+        System.out.println("\nRemoving the top of the stack: " + s1.pop());
+        System.out.println("\nSize of the stack : " + s1.sSize());
+
     }
 }
